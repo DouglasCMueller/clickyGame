4026e53a0383406fe96a75be9af856d309dd0b37
Douglas Mueller <douglasmueller6@gmail.com>
2020-03-10 12:17:53 -0400

    initial commit - functional react app created

---
+++ clickygame/src/components/InstructionsContent/index.js
@@ -0,0 +1,14 @@
+import React from "react"; 
+import "./style.css";
+
+function InstructionContainer() {
+  return (
+    <div className = "instructionContainer">
+        <p className="paragraphTitle">Clicky Game!</p>
+        <p className="paragraphInstructions">Click on an image to earn points, but don't click on any more than once!</p>
+</div>
+
+  );
+}
+
+export default InstructionContainer;
